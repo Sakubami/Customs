fcbac80283db876978f3f3caba9ee1d4504ce6ba
BuildTools <louiswaechter1994@gmail.com>
2023-09-11 23:00:39 +0200

    Implemented Economy and Owner

--- src/main/java/xyz/samiki/zollsystem/listeners/InventoryClick.java
+++ src/main/java/xyz/samiki/zollsystem/listeners/InventoryClick.java
@@ -1,6 +1,8 @@
 package xyz.samiki.zollsystem.listeners;
 
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
@@ -11,6 +13,7 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.ItemStack;
 import xyz.samiki.zollsystem.ConfigHelper;
 import xyz.samiki.zollsystem.ZollSystem;
+import xyz.samiki.zollsystem.controllers.ChatController;
 import xyz.samiki.zollsystem.controllers.PlaceController;
 
 public class InventoryClick implements Listener {
@@ -26,18 +29,42 @@ public class InventoryClick implements Listener {
 
             if (item != null) {
                 if (item.getItemMeta().getDisplayName().equalsIgnoreCase("§aJa")) {
+                    Location loc = ConfigHelper.getLocByStatus(p);
+                    double price = ConfigHelper.getPrice(loc);
+                    double balance = ZollSystem.getEconomy().getBalance(p);
+                    String owner = ConfigHelper.getOwner(loc);
 
-                    ConfigHelper.setBusy(ConfigHelper.getLocByStatus(p), true);
-                    PlaceController.open(p);
-                    p.playSound(p, Sound.BLOCK_NOTE_BLOCK_HARP, 21 , 1);
-                    Bukkit.getScheduler().runTaskLater(ZollSystem.getPlugin(), bukkitTask -> {
+                    if (balance > price || balance == price) {
+                        if (owner != null && ZollSystem.getEconomy().hasAccount(owner)) {
 
-                        PlaceController.close(p);
-                        ConfigHelper.setBusy(ConfigHelper.getLocByStatus(p), false);
-                        ConfigHelper.setStatus(ConfigHelper.getLocByStatus(p), p, false);
+                            Player ownerPlayer = Bukkit.getServer().getPlayer(owner);
 
-                    },100);
-                    // take money
+                            p.playSound(p, Sound.BLOCK_NOTE_BLOCK_HARP, 21 , 1);
+                            ZollSystem.getEconomy().withdrawPlayer(p, price);
+
+                            p.sendMessage(ChatController.generic("Dir wurden §c" + price + "$ §7Berechnet"));
+
+                            if (ownerPlayer != null) {
+                                ownerPlayer.sendMessage(ChatController.generic("+ §c" + price + "$ §7Zoll"));
+                            }
+
+                            ConfigHelper.setBusy(loc, true);
+                            PlaceController.open(p);
+
+                            Bukkit.getScheduler().runTaskLater(ZollSystem.getPlugin(), bukkitTask -> {
+
+                                PlaceController.close(p);
+                                ConfigHelper.setBusy(loc, false);
+                                ConfigHelper.setStatus(loc, p, false);
+
+                            },100);
+                        } else {
+                            p.sendMessage(ChatController.error("§4FEHLER: §cDiese Pforte hat einen ungültigen Owner! Bitte kontaktiere einen Developer - §d(saku / sakubami) §c- per Ticket im Discord mit einem Screenshot dieser Nachricht und deinen Coordinaten"));
+                        }
+                    } else {
+                        p.sendMessage(ChatController.error("Du hast nicht genügend Geld"));
+                    }
+
                     e.getView().close();
                     e.setCancelled(true);
                 }
@@ -62,8 +89,9 @@ public class InventoryClick implements Listener {
     public void makeButtonsDoStuff(InventoryCloseEvent e) {
         Player p = (Player) e.getPlayer();
         if (e.getView().getTitle().equalsIgnoreCase("§4Kostenpflichtig §8Passieren?")) {
-            if (ConfigHelper.checkBusy(ConfigHelper.getLocByStatus(p))) {
-                ConfigHelper.setStatus(ConfigHelper.getLocByStatus(p), p, false);
+            Location loc = ConfigHelper.getLocByStatus(p);
+            if (ConfigHelper.checkBusy(loc)) {
+                ConfigHelper.setStatus(loc, p, false);
             }
         }
     }
