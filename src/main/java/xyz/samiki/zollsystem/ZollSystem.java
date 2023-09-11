5620fb69065984d1e1b561ad82c54f7f4839e903
BuildTools <louiswaechter1994@gmail.com>
2023-09-11 22:58:14 +0200

    Implemented Economy

--- src/main/java/xyz/samiki/zollsystem/ZollSystem.java
+++ src/main/java/xyz/samiki/zollsystem/ZollSystem.java
@@ -1,13 +1,16 @@
 package xyz.samiki.zollsystem;
 
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.samiki.zollsystem.commands.ZollCommand;
 import xyz.samiki.zollsystem.listeners.Interact;
 import xyz.samiki.zollsystem.listeners.InventoryClick;
 
 public class ZollSystem extends JavaPlugin {
+    private static Economy econ = null;
     private static ZollSystem plugin;
 
     public void onEnable(){
@@ -18,13 +21,31 @@ public class ZollSystem extends JavaPlugin {
         pm.registerEvents(new InventoryClick(), this);
 
         getCommand("zoll").setExecutor(new ZollCommand());
+
+        if (!setupEconomy() ) {
+            getServer().getPluginManager().disablePlugin(this);
+            System.out.println("DISABLED DUE TO NO VAULT FOUND");
+        }
     }
 
     public void onDisable(){
 
     }
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return true;
+    }
 
     public static ZollSystem getPlugin() {
         return plugin;
     }
-}
+    public static Economy getEconomy() { return econ; }
+
+    }
